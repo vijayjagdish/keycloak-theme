f4f6df28e4f5da2a9eaeb3d57d481ac54d1ef6a6
vijayjagadish <vijayj@creamcollar.com>
2024-01-24 19:15:05 +0530

    message and eye icon

--- login/resources/js/login.js
+++ login/resources/js/login.js
@@ -34,16 +34,19 @@ window.onload = function () {
 
     if(document.getElementById("password_icon")){
     var password_icon = document.getElementById("password_icon");
+    var password = document.getElementById('password');
     password_icon.addEventListener('click', function(){
         if(password_icon.value==0)
         {
             password_icon.classList.remove('password_hide_icon');
             password_icon.classList.add('password_show_icon');
+            password.type='password';
             password_icon.value=1;
         }
         else{
             password_icon.classList.remove('password_show_icon');
             password_icon.classList.add('password_hide_icon');
+            password.type='text';
             password_icon.value=0;
         }
     });
